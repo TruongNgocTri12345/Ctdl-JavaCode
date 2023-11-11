548c6472c039dd71fc37a9f17a7b2bf462334c76
TruongNgocTri12345 <truongngoctri12345@gmail.com>
2023-11-11 22:40:48 +0700

    Ctdl_Java Project

---
+++ Lab_08/TestReadFile.java
@@ -0,0 +1,17 @@
+package Lab_08;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TestReadFile {
+	public static void main(String[] args) throws FileNotFoundException {
+		// Scanner input = new Scanner(new File("data/hamlet.txt"));
+		Scanner input = new Scanner(new File("data/fut.txt"));
+
+		while (input.hasNext()) {
+			String word = input.next();
+			System.out.println(word);
+		}
+	}
+}
